fdf1241008b2cacac8253ec27fc59ff73c5d938e
Helvio Siqueira <67134795+HelvioSiqueira@users.noreply.github.com>
2022-08-03 16:19:53 -0300

    Criando e configurado InicioFragment e InicioActivity

---
+++ app/src/main/java/com/example/cadastro/InicioActivity.kt
@@ -0,0 +1,24 @@
+package com.example.cadastro
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class InicioActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_inicio)
+
+        if(savedInstanceState == null){
+            showInicioFragment()
+        }
+    }
+
+    private fun showInicioFragment(){
+        val fragment = InicioFragment.newInstance()
+
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.inicio, fragment)
+            .commit()
+    }
+}
---
+++ app/src/main/java/com/example/cadastro/InicioFragment.kt
@@ -0,0 +1,29 @@
+package com.example.cadastro
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class InicioFragment: Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.activity_main, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    companion object{
+
+        private val TAG_INICIO = "tagInicio"
+
+        fun newInstance() = InicioFragment()
+    }
+}
